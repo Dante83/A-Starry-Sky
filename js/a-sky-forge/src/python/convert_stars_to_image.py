79d99b31a02b5bd1544ee5f3793eb89ccf0b0820
unknown <david.relativitytheory@gmail.com>
2018-02-23 12:47:27 -0700

    Forgot to build out our conversion between floats and rgba format. Fun. Also corrected some other obvious bugs that were visible and added some tests to get some visibility into this entire process.

--- js/a-sky-forge/src/python/convert_stars_to_image.py
+++ js/a-sky-forge/src/python/convert_stars_to_image.py
@@ -4,7 +4,7 @@
 import csv, os, json, math
 import numpy as np
 import Image
-import parse_visible_stars_to_json
+import parse_visible_stars_to_json as pvs
 
 def approximatePosition(targetWidth, targetHeight, star_ra, star_dec):
     #First let's just try a linear map and presume that our data points are distributed evenly...
@@ -12,14 +12,40 @@ def approximatePosition(targetWidth, targetHeight, star_ra, star_dec):
     bestYPos = star_dec * targetHeight / pi
     return [bestXPos, bestYPos]
 
-def float2RGBA(floatNumber):
-    #This converts any given number into it's corresponding RGBA
-    hex(struct.unpack('<I', struct.pack('<f', f))[0])
-    red = 0.0
-    green = 0.0
-    blue = 0.0
-    alpha = 0.0
-    return (red, green, blue, alpha)
+def float2RGBA(floatNumber, maxNum, minNum):
+    #Perform a transformation into the space between 0.0 and 1.0
+    if(minNum >= 0.0):
+        normalizedFloatingNumber = (floatNumber - minNum) / (maxNum - minNum)
+    else:
+        normalizedFloatingNumber = (floatNumber + minNum) / (maxNum - minNum)
+
+    #Check that all of our math is still right, we don't want anything going outside the bounds
+    if(normalizedFloatingNumber > 1.0):
+        print normalizedFloatingNumber
+        normalizedFloatingNumber = 1.0
+    elif(normalizedFloatingNumber < 0.0):
+        print normalizedFloatingNumber
+        normalizedFloatingNumber = 0.0
+
+    #Now do our conversion between this normalized float and RGBA
+    #With a little help from https://aras-p.info/blog/2009/07/30/encoding-floats-to-rgba-the-final/
+    encoding = np.array(1.0,255.0,65025.0,16581375.0) * normalizedFloatingNumber
+    encoding = np.modf(encoding)[0]
+    encoding -= np.array(encoding[1], encoding[2], encoding[3], encoding[3]) * np.array(1.0/255.0, 1.0/255.0, 1.0/255.0, 0.0)
+
+    red = encoding[0];
+    green = encoding[1];
+    blue = encoding[2];
+    alpha = encoding[3];
+
+    #Test that this actually works
+    decoded = np.dot(encoding, np.array(1.0, 1.0/255.0, 1.0/65025.0, 1.0/16581375.0))
+    if(decoded == normalizedFloatingNumber):
+        print 'It worked!'
+    else:
+        print "The number %d does not equal the number %d" % (decoded, normalizedFloatingNumber)
+
+    return [red, green, blue, alpha]
 
 def sortByDistanceFromOriginalPixel(choice, target):
     return ((a[0] - target[0]) ** 2 + (a[1] - target[0]) ** 2) ** (0.5)
@@ -29,9 +55,9 @@ def recursiveTuple(tupleThisList):
     for item in tupleThisList:
         if type(H) is list:
             tupledList = recursiveTuple(item)
-            replacementList = replacementList + [tupledList]
+            replacementList += [tupledList]
         else:
-            replacementList + [tupledList]
+            replacementList += [tupledList]
     return replacementList(tuple(replacementList))
 
 def main():
@@ -49,7 +75,7 @@ def main():
             #Exclude the SUN! >_<
             if magnitude <= 6.5 and magnitude > -26:
                 if star['ci']:
-                    star_color = bv2rgb(float(star['ci']))
+                    star_color = pvs.bv2rgb(float(star['ci']))
                 else:
                     star_color = {'red': 1.0, 'green': 1.0, 'blue': 1.0}
 
@@ -60,6 +86,17 @@ def main():
                 'color': star_color}
 
                 star_data += [data_point]
+
+    #Get our maximum and minimum data values so that we convert everything into a range between 0.0 and 1.0
+    maxRa = 2.0 * pi
+    minRa = 0.0
+    maxDec = -pi / 2.0
+    minDec = pi / 2.0
+    maxMag = max([star.magnitude for star in star_data])
+    minMag = min([star.magnitude for star in star_data])
+
+    #Also, print these out because we're probably going to want these values in our fragment shader
+    print 'The maximum magnitude is %d and the minimum magnitude is %d' % (maxMag, minMag)
 
     #Organize our stars from brightest to dimmest so that we grant the brightest stars priority placement.
     sorted_star_data = sorted(star_data, key=lambda star: star.magnitude)
@@ -96,11 +133,11 @@ def main():
             pixels_in_ring = []
             pixel_of_ring = 0
             for x in range(startingPosition.x - radial_offset, startingPosition.x + radial_offset):
-                pixels_in_ring = pixels_in_ring + [[x, startingPosition.y + radial_offset]]
-                pixels_in_ring = pixels_in_ring + [[x, startingPosition.y - radial_offset]]
+                pixels_in_ring += [[x, startingPosition.y + radial_offset]]
+                pixels_in_ring += [[x, startingPosition.y - radial_offset]]
             for y in range(startingPosition.y - radial_offset + 1, startingPosition.y + radial_offset - 1):
-                pixels_in_ring = pixels_in_ring + [[startingPosition.x + radial_offset, y]]
-                pixels_in_ring = pixels_in_ring + [[startingPosition.y + radial_offset, y]]
+                pixels_in_ring += [[startingPosition.x + radial_offset, y]]
+                pixels_in_ring += [[startingPosition.y + radial_offset, y]]
             sorted_pixels_in_ring = sort(pixels_in_ring, key=lambda position, target=approximatePosition: sortByDistanceFromOriginalPixel(position, target))
             while(radial_offset <= 5):
                 next_pixel = sorted_pixels_in_ring[pixel_of_ring]
@@ -115,11 +152,11 @@ def main():
                     pixels_in_ring = []
                     distances_to_each_pixel = []
                     for x in range(startingPosition.x - radial_offset, startingPosition.x + radial_offset):
-                        pixels_in_ring = pixels_in_ring + [[x, startingPosition.y + radial_offset]]
-                        pixels_in_ring = pixels_in_ring + [[x, startingPosition.y - radial_offset]]
+                        pixels_in_ring += [[x, startingPosition.y + radial_offset]]
+                        pixels_in_ring += [[x, startingPosition.y - radial_offset]]
                     for y in range(startingPosition.y - radial_offset + 1, startingPosition.y + radial_offset - 1):
-                        pixels_in_ring = pixels_in_ring + [[startingPosition.x + radial_offset, y]]
-                        pixels_in_ring = pixels_in_ring + [[startingPosition.y + radial_offset, y]]
+                        pixels_in_ring += [[startingPosition.x + radial_offset, y]]
+                        pixels_in_ring += [[startingPosition.y + radial_offset, y]]
                     sorted_pixels_in_ring = sort(pixels_in_ring, key=lambda position, target=approximatePosition: sortByDistanceFromOriginalPixel(position, target))
             #Well, either we found it or it got crowded out by brighter stars in the sky
             if(radial_offset <= 5):
@@ -142,9 +179,9 @@ def main():
                 print "The star with id, " + star_array[x][y] + " was not found?! Blasphemy!"
             else:
                 star_array[x][y] = float2RGBA(desired_star.ra)
-                star_array[x + image_width][y] = float2RGBA(desired_star.ra)
-                star_array[x + (2.0 * image_width)][y] = float2RGBA(desired_star.dec)
-                star_array[x + (3.0 * image_width)][y] = float2RGBA(desired_star.mag)
+                star_array[x + image_width][y] = float2RGBA(desired_star.ra, maxRa, minRa)
+                star_array[x + (2.0 * image_width)][y] = float2RGBA(desired_star.dec, maxDec, minDec)
+                star_array[x + (3.0 * image_width)][y] = float2RGBA(desired_star.mag, maxMag, minMag)
                 star_array[x + (4.0 * image_width)][y] = [ceil(desired_star.r * 255.0), ceil(desired_star.r * 255.0),ceil(desired_star.r * 255.0)]
 
     #Convert this array into a png that we can later import and use to populate our stars at key points
